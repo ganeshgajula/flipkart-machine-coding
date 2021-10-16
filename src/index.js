70847393e779e6a5b2c2dbd2ff8a8b3ac617364a
Ganesh Gajula <ganeshgajula@Ganeshs-MacBook-Air.local>
2021-10-16 19:59:36 +0530

    add: context wrapper

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
+import { ProductsProvider } from "./context/ProductsProvider";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <ProductsProvider>
+        <App />
+      </ProductsProvider>
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
